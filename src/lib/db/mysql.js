d1bd50570ebb48cacfdc73633e7c9d1e7c06511c
Gani Hasmegaj <ganhas20@htl-shkoder.com>
2025-01-22 15:25:37 +0100

    test

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -6,7 +6,7 @@ export function createConnection() {
 		connection = mysql.createConnection({
 			host: 'htl-projekt.com',
 			user: 'ganihasmegaj',
-			port: 3306,
+			port: 33060,
 			password: '!Insy_2024$',
 			database: '2024_4bw_ganihasmegaj_event_app'
 		});
